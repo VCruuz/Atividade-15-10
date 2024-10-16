531fa14b92d569ddb1900249cf45d9f0e7400edd
Vitor Cruz <vitorcruz2510@gmail.com>
2024-10-15 22:09:05 -0300

    commit inicial

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Carro carro = new Carro();
+        carro.acelerar();
+        carro.frear();
+    
+        Moto moto = new Moto();
+        moto.acelerar();
+        moto.frear();
+        moto.Empinar();
+    }
+
+}
---
+++ src/Moto.java
@@ -0,0 +1,15 @@
+class Moto implements Veiculo{
+    @Override
+    public void acelerar(){
+        System.out.println("A Moto está acelerando.");
+    }
+
+    @Override
+    public void frear(){
+        System.out.println(" A Moto está freando. ");
+    }
+
+    public void Empinar(){
+        System.out.println(" A Moto está Empinando. ");
+    }
+}
